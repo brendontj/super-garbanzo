6792ed556c368b6b3ec43d12858456cf597e4635
Brendon Henrique <68242224+brendontj@users.noreply.github.com>
2023-08-24 21:51:16 -0300

    feat: finish the read of log file and the parse log data  (#1)

---
+++ core/domain/game/game.go
@@ -0,0 +1,63 @@
+package game
+
+type Game struct {
+	ID               int
+	TotalKills       int
+	Kills            map[string]int
+	DeathCauseRecord map[string]int
+	WasClosed        bool
+}
+
+func NewGame(gameIdentifier int) Game {
+	return Game{
+		ID:               gameIdentifier,
+		TotalKills:       0,
+		Kills:            make(map[string]int, 0),
+		DeathCauseRecord: make(map[string]int, 0),
+	}
+}
+
+func (g *Game) RegisterKill(killer, death, reason string) {
+	g.TotalKills += 1
+	_, exist := g.DeathCauseRecord[reason]
+	if !exist {
+		g.DeathCauseRecord[reason] = 1
+		return
+	}
+	g.DeathCauseRecord[reason] += 1
+
+	if killer == "<world>" {
+		g.removeKillFromPlayer(death)
+		return
+	}
+
+	g.addKillToPlayer(killer)
+}
+
+func (g *Game) Players() []string {
+	playerNames := make([]string, 0, len(g.Kills))
+	for k := range g.Kills {
+		playerNames = append(playerNames, k)
+	}
+	return playerNames
+}
+
+func (g *Game) addKillToPlayer(playerName string) {
+	_, exist := g.Kills[playerName]
+	if !exist {
+		g.Kills[playerName] = 1
+		return
+	}
+
+	g.Kills[playerName] += 1
+}
+
+func (g *Game) removeKillFromPlayer(playerName string) {
+	_, exist := g.Kills[playerName]
+	if !exist {
+		g.Kills[playerName] = -1
+		return
+	}
+
+	g.Kills[playerName] -= 1
+}
--- main.go
+++ main.go
@@ -1,5 +1,64 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"github.com/brendontj/super-ganbanzo/core/domain/records"
+	"os"
+	"sync"
+	"time"
+)
+
 func main() {
-	panic("implement me")
+	filePath := os.Args[1]
+
+	dataStream := make(chan string, 10000)
+	closeCh := make(chan struct{})
+	gr := records.NewGameRecords()
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go ReadFileByLine(&wg, filePath, dataStream, closeCh)
+	go ParseFromDataStream(&wg, &gr, dataStream, closeCh)
+	wg.Wait()
+
+	fmt.Println(gr)
+}
+
+func ReadFileByLine(wg *sync.WaitGroup, filePath string, data chan string, closeCh chan struct{}) {
+	defer wg.Done()
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		data <- scanner.Text()
+	}
+	close(data)
+	closeCh <- struct{}{}
+	close(closeCh)
+}
+
+func ParseFromDataStream(wg *sync.WaitGroup, gr *records.GameRecords, dataStream chan string, closeCh chan struct{}) {
+	defer wg.Done()
+	//var dataBufferInMemory []string
+
+	for {
+		select {
+		case data := <-dataStream:
+			gr.ParseRecord(data)
+
+			//dataBufferInMemory = append(dataBufferInMemory, data)
+
+		case <-closeCh:
+			if len(dataStream) != 0 {
+				continue
+			}
+			//fmt.Println(dataBufferInMemory)
+			fmt.Println("Finishing program execution")
+			return
+		default:
+			time.Sleep(time.Millisecond * 50)
+		}
+	}
 }
